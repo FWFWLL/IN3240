8f8031dcf06af3ca820200fc86ec907022088d8f
FWFWLL <ffl@vivaldi.net>
2023-03-13 13:11:34 +0100

    Finish Task 2.5

--- cypress/e2e/oblig-1.cy.js
+++ cypress/e2e/oblig-1.cy.js
@@ -6,16 +6,12 @@ context.skip("Task 1.5 - Automated Testing of Customer Care", () => {
 	});
 
 	specify("Task 1.5 - Test customer care", () => {
-		// Fill form
 		cy.get("#name").type("Vestia Zeta");
 		cy.get("#email").type("giguvosnar@vivaldi.net");
 		cy.get("#phone").type("98091984");
 		cy.get("#message").type("Have you heard of the critically acclaimed MMORPG Final Fantasy XIV?");
 
-		// Click submit button
 		cy.get(".button[value=\"Send to Customer Care\"]").click();
-
-		// Expected result
 		cy.contains("Thank you");
 	});
 });
@@ -27,11 +23,9 @@ context("Task 2.5 - Automated Testing of Website", () => {
 	beforeEach(() => {
 		cy.visit("https://parabank.parasoft.com/parabank/index.htm");
 
-		// Fill login form
 		cy.get(".input[name=\"username\"]").type(username);
 		cy.get(".input[name=\"password\"]").type(password);
 
-		// Log in
 		cy.get(".button[value=\"Log In\"]").click();
 	});
 
@@ -39,27 +33,44 @@ context("Task 2.5 - Automated Testing of Website", () => {
 		cy.get("a[href=\"/parabank/logout.htm\"]").click();
 	});
 
-	specify.skip("Test 2.5.1 - Open New Account", () => {
-		// Navifate to Open New Account Page
+	specify("Test 2.5.1 - Open New Account", () => {
 		cy.get("a[href=\"/parabank/openaccount.htm\"]").click();
 
-		// Select account type
+		cy.wait(1000);
+
 		cy.get("#type").select(1);
+		cy.get("#fromAccountId").select(0);
 
-		// Select existing account
-		cy.get("#fromAccountId").select(1);
-
-		// Open New Account
 		cy.get(".button[value=\"Open New Account\"]").click();
 
-		// Expected Result
-		cy.contains("Congratulations, your account is now open.");
+		cy.contains("Account Opened!");
 	});
 
-	specify("Task 2.5.2 - ", () => {
+	specify("Task 2.5.2 - Transfer Funds", () => {
+		cy.get("a[href=\"/parabank/transfer.htm\"]").click();
+
+		cy.wait(1000);
+
+		cy.get("#amount").type("69");
+		cy.get("#fromAccountId").select(0);
+		cy.get("#toAccountId").select(1);
+
+		cy.get(".button[value=\"Transfer\"]").click();
+
+		cy.contains("Transfer Complete!");
 	});
 
-	specify.skip("Test 2.5.3 - ", () => {
+	specify("Test 2.5.3 - Request Loan", () => {
+		cy.get("a[href=\"/parabank/requestloan.htm\"]").click();
 
+		cy.wait(1000);
+
+		cy.get("#amount").type("69");
+		cy.get("#downPayment").type("69");
+		cy.get("#fromAccountId").select(0);
+
+		cy.get(".button[value=\"Apply Now\"]").click();
+
+		cy.contains("Loan Request Processed");
 	});
 });
